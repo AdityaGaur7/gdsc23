963c6244aa973c54580d6cdd6bf86d5daac29158
AdityaGaur7 <adgaur027@gmail.com>
2024-02-06 02:09:43 +0530

    loader added

---
+++ src/components/home/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Loader.css"
+function Loader() {
+  return (
+    <div className='loader'>
+      <img src="loader.gif" alt="" />
+      
+    </div>
+  )
+}
+
+export default Loader
