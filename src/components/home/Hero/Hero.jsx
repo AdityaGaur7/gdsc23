804f14cbc6ac1642d3629a8a28b42b0a13bb0dc7
AdityaGaur7 <adgaur027@gmail.com>
2024-02-05 20:17:46 +0530

    new commit

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -1,7 +1,8 @@
 import Button from '../../common/button/Button'
 import './Hero.css'
 import Card from '../section1/Card'
-
+import Service from "../Section2/Service"
+import About from "../About/About"
 export default function Hero() {
     return (
 <>
@@ -10,13 +11,15 @@ export default function Hero() {
         <img className='heroright' src='heroright.png' alt='heroright' />
         <div className="wrapper">
             <img src='/shortlogo.svg' alt='short logo' />
-            <h2>Google Developer Student Club</h2>
+            <h2>Google Developer <br/>Student Club</h2>
             <p>MMMUT</p>
             <Button text={'Join Us'} />
             <img src="image1/homegif.gif" className='gif2' alt="gif2" />
         </div>
     </div>
+    <About/>
     <Card/>
+    <Service/>
     </>
     )
 }   
---
+++ src/components/home/Section2/Service.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Service.css"
+function Service() {
+  return (
+    <>
+    <div className='service'>
+        <div className="content">
+        <img src="/image1/bootcamp.png" alt="" />
+      <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium sequi modi impedit, laudantium rem eius sit maxime, cupiditate dignissimos blanditiis voluptate ut tempora obcaecati hic repudiandae libero odio laborum perspiciatis quos non consectetur! Consequuntur quidem obcaecati blanditiis molestiae saepe vitae!</h2>
+      </div>
+     
+    </div>
+    </>
+  )
+}
+
+export default Service
