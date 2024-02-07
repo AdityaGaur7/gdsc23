a7baa4f7d64318982124c4a60569b33e9cd24536
Pranjul Mani <pranjulmani010@gmail.com>
2024-02-07 15:20:18 +0530

    contact page added

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,45 @@
+import './Contact.css'
+export default function Contact() {
+    return (
+        <>
+        
+        <div className="hanging-words-container">
+    <span className="hanging-word"><img src="img/g.svg" alt=""/></span>
+    <span className="hanging-word"><img src="img/d.svg" alt=""/> </span>
+    <span className="hanging-word"><img src="img/s.svg" alt=""/></span>
+    <span className="hanging-word"><img src="img/c.svg" alt=""/> </span>
+  </div>
+  <div>
+    <h1>Find us on</h1>
+    <ul className="flex">
+      <li>
+        <a className="decor" href="/">
+          <img src="img/insta.svg" alt="instagram-icon"/>
+          <span id="insta">Instagram</span>
+        </a>
+      </li>
+      <li>
+        <a className="decor" href="/">
+          <img src="img/linkedin.svg" alt="linkedin-icon"/>
+          <span id="link">LinkedIn</span>
+        </a>
+      </li>
+      <li>
+        <a className="decor" href="/">
+          <img src="img/telegram.svg" alt="instagram-icon"/>
+          <span id="tele">Telegram</span>
+        </a>
+      </li>
+      <li>
+        <a className="decor" href="/">
+          <img src="img/x.svg" alt="instagram-icon"/>
+          <span id="twit">Twitter</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+        
+        
+        </>
+    )
+}
