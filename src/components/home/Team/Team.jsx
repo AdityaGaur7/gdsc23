3eae56acb13cc5878e628c33955d037d1384d77a
AdityaGaur7 <adgaur027@gmail.com>
2024-03-04 00:56:53 +0530

    ok

--- src/components/home/Team/Team.jsx
+++ src/components/home/Team/Team.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import "./Team.css";
-function Team() {
-  return (
-    <div>
-      <h1>Jyoti your team page goes here</h1>
-    </div>
-  )
-}
+import React from 'react';
+import teamData from './TeamInfo.json';
 
-export default Team
+const TeamPage = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-4">Our Team</h1>
+      <div className="row">
+        {teamData.map((member, index) => (
+          <div key={index} className="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div className="card">
+              <img src={member.image} className="card-img-top" alt={member.name} />
+              <div className="card-body">
+                <h5 className="card-title">{member.name}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TeamPage;
